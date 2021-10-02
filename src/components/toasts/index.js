5a32579a6fe7d5d8237810125c8cea12213876ee
Jukka Koivu <skuuppi11@hotmail.com>
2021-10-02 21:27:28 +0300

    Add recepies
    Adding recipies to the database is now working

--- src/components/toasts/index.js
+++ src/components/toasts/index.js
@@ -1,5 +1,6 @@
 import Toast from "react-native-toast-message"
 
+// Authentication toasts
 export const loginSuccToast = () => {
   Toast.show({
     type: "success",
@@ -33,5 +34,24 @@ export const registerSuccToast = () => {
       position: "bottom",
       text1: "Passwords not matching",
       text2: "Try checking your passwords",
+    })
+  }
+
+  // Recipe toasts
+  export const addRecipeToast = () => {
+    Toast.show({
+      type: "success",
+      position: "bottom",
+      text1: "Success",
+      text2: "Your recipe was added succesfully"
+    })
+  }
+
+  export const addRecipeErrorToast = () => {
+    Toast.show({
+      type: "error",
+      position: "bottom",
+      text1: "Something went wrong",
+      text2: "Something went wrong while adding your recipe"
     })
   }
--- src/screens/recepies/addRecepies.js
+++ src/screens/recepies/addRecepies.js
@@ -1,48 +1,117 @@
-import React from "react"
+import React, { useState } from "react"
 import { View, StyleSheet, Text, Pressable, Dimensions } from "react-native"
 import { Input } from "react-native-elements"
+import Ionicons from "react-native-vector-icons/Ionicons"
+import { firebase } from "../../firebase/config"
+// Toasts
+import { addRecipeToast, addRecipeErrorToast } from '../../components/toasts'
 // colors
 import colors from "../../styles/colors"
 
-const width = Dimensions.get('screen').width
+const width = Dimensions.get("screen").width
 
 const AddRecipeScreen = () => {
+  const [recipe, setRecipe] = useState('')
+  const [course, setCourse] = useState('')
+  const [preptime, setPreptime] = useState('')
+  const [cookingtime, setCookingtime] = useState('')
+  const [servings, setServings] = useState('')
+  const [ingredients, setIngredients] = useState([])
+
+  const addRecipe = () => {
+    const uid = firebase.auth().currentUser.uid
+    firebase
+    .database()
+    .ref(`Users/${uid}/recepies/`)
+    .push({
+      recipe: recipe,
+      course: course,
+      preptime: preptime,
+      cookingtime: cookingtime,
+      servings: servings
+    })
+    .then(() => {
+      addRecipeToast()
+    })
+    .catch((error) => {
+      addRecipeErrorToast()
+    })
+  } 
+
   const {
     container,
+    addRecipeForm,
     inputContainer,
     inputTimeContainer,
+    inputIngredientContainer,
     inputStyle,
     inputGroup,
+    inputAddGroup,
     addButton,
     buttonContainer,
-    buttonLabel
+    buttonLabel,
+    addIcon,
   } = styles
   return (
     <View style={container}>
+      <View style={addRecipeForm}>
       <Input
         inputContainerStyle={inputContainer}
         placeholder="Name of the recipe"
         placeholderTextColor={colors.white}
+        onChangeText={(text) => setRecipe(text)}
+        value={recipe}
+      />
+      <Input
+        inputContainerStyle={inputContainer}
+        placeholder="Course"
+        placeholderTextColor={colors.white}
+        onChangeText={(text) => setCourse(text)}
+        value={course}
       />
       <View style={inputGroup}>
         <Input
           inputContainerStyle={inputTimeContainer}
           inputStyle={inputStyle}
-          placeholder="Cooking time"
+          placeholder="Preparation time"
           placeholderTextColor={colors.white}
+          onChangeText={(text) => setPreptime(text)}
+          value={preptime}
         />
         <Input
           inputContainerStyle={inputTimeContainer}
           inputStyle={inputStyle}
-          placeholder="Total time"
+          placeholder="Cooking time"
           placeholderTextColor={colors.white}
+          onChangeText={(text) => setCookingtime(text)}
+          value={cookingtime}
         />
       </View>
-      <Pressable style={addButton}>
+      <Input
+        inputContainerStyle={inputContainer}
+        placeholder="Servings"
+        placeholderTextColor={colors.white}
+        onChangeText={(text) => setServings(text)}
+        value={servings}
+      />
+      <View style={inputAddGroup}>
+        <Input
+          inputContainerStyle={inputIngredientContainer}
+          inputStyle={inputStyle}
+          placeholder="Ingredient"
+          placeholderTextColor={colors.white}
+          onChangeText={(text) => setIngredients(text)}
+        />
+        <Pressable onPress={() => {}}>
+          <Ionicons style={addIcon} name="ios-add-circle-outline" size={50} />
+        </Pressable>
+      </View>
+      <Pressable style={addButton} onPress={() => addRecipe()}>
         <View style={buttonContainer}>
-        <Text style={buttonLabel}>Add recipe</Text>
+          <Text style={buttonLabel}>Add recipe</Text>
         </View>
       </Pressable>
+      </View>
     </View>
   )
 }
@@ -54,6 +123,9 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignContent: "center",
   },
+  addRecipeForm: {
+    marginTop: - 150
+  },
   inputContainer: {
     borderBottomColor: colors.black,
   },
@@ -61,10 +133,21 @@ const styles = StyleSheet.create({
     borderBottomColor: colors.black,
     width: "50%",
   },
+  inputIngredientContainer: {
+
+    borderBottomColor: colors.black,
+    width: "85%",
+  },
   inputGroup: {
     flexDirection: "row",
     justifyContent: "space-evenly",
     marginLeft: 105,
+  },
+  inputAddGroup: {
+    
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    marginLeft: 35,
   },
   inputStyle: {
     width: 200,
@@ -73,19 +156,23 @@ const styles = StyleSheet.create({
     backgroundColor: colors.blue,
     width: width / 1.2,
     height: 50,
-    alignSelf: 'center',
-    borderRadius: 9
+    alignSelf: "center",
+    borderRadius: 9,
   },
   buttonContainer: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+    justifyContent: "center",
+    alignItems: "center",
   },
   buttonLabel: {
     color: colors.white,
     fontSize: 20,
-    textAlign: 'center'
-  }
+    textAlign: "center",
+  },
+  addIcon: {
+    color: colors.white,
+    marginRight: 50,
+  },
 })
 
 export default AddRecipeScreen
